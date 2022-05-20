569877b67b2c87b0183180b688fd43b781f7d753
fabiolaroca <fhblaroca@gmail.com>
2022-05-19 21:17:29 -0300

    primeira prática dia 3 - terceira parte

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -4,4 +4,10 @@ let profissão = "Psicólogo"
 let idade = 31
 let sonho = "Tóquio"
 
+console.log("Nome:", nome) 
+console.log("E-mail:", email) 
+console.log("Profissão:", profissão) 
+console.log("Idade:", idade) 
+console.log("Sonho de Viagem:", sonho)
 
+console.log("Nome:", nome, "E-mail:", email, "Profissão:", profissão, "Idade:", idade, "Sonho de Viagem:", sonho)
