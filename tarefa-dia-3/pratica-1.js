6f94bfdf3c9cea1accd72669599e3e71ab60a44b
fabiolaroca <fhblaroca@gmail.com>
2022-05-19 21:23:45 -0300

    primeira prática dia 3 - desafio

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -11,3 +11,5 @@ console.log("Idade:", idade)
 console.log("Sonho de Viagem:", sonho)
 
 console.log("Nome:", nome, "E-mail:", email, "Profissão:", profissão, "Idade:", idade, "Sonho de Viagem:", sonho)
+
+console.log("Olá,", nome,"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para", sonho, ". Confirma pra gente alguns dados? A sua idade é", idade, ", você é", profissão, "e o seu email é", email, "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
